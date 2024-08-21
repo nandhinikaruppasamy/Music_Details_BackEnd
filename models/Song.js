3aacc7c66f8a392ecb80686311e2899c3bdf8bea
Nandhini <111649724+nandhinikaruppasamy@users.noreply.github.com>
2024-08-21 16:23:18 +0530

    Update Song.js

--- models/Song.js
+++ models/Song.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+const Playlist = require('./Playlist');
 
 const SongSchema = new mongoose.Schema({
   title: { type: String, required: true },
+  playlist_id:{
+        type: mongoose.Schema.Types.ObjectId,
+    ref: 'Playlist'
+  },
   artist: { type: String, required: true },
   album: { type: String, required: true },
   duration: { type: String, required: true },
