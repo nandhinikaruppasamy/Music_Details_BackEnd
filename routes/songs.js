df937a7815cd306456634fd0071222b09d6a97db
Nandhini <111649724+nandhinikaruppasamy@users.noreply.github.com>
2024-08-21 16:22:12 +0530

    Update songs.js

--- routes/songs.js
+++ routes/songs.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
 const Song = require('../models/Song');
+const Playlist = require('../models/Playlist'); 
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-  const { title, artist, album, duration, url } = req.body;
-
+router.post('/add', async (req, res) => {
   try {
-    const song = new Song({ title, artist, album, duration, url });
+    const { title, playlist_id, artist, album, duration, url } = req.body;
+
+    const playlist = await Playlist.findById(playlist_id);
+    if (!playlist) {
+      return res.status(400).json({ msg: 'Playlist not found' });
+    }
+    const song = new Song({ title, playlist_id, artist, album, duration, url });
     await song.save();
     res.status(201).json(song);
   } catch (err) {
@@ -16,34 +21,22 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.get('/', async (req, res) => {
+router.get('/read', async (req, res) => {
   try {
-    const songs = await Song.find();
-    res.json(songs);
+     const pid=req.query.pid;
+     if(!pid)
+
+{
+  return res.status(400).json({ msg: 'Playlist ID is required' });
+
+}   else{   const songs = await Song.find({playlist_id:pid});
+    res.json(songs);}
   } catch (err) {
     res.status(500).send('Server error');
   }
 });
 
-router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ msg: 'Invalid song ID' });
-  }
-
-  try {
-    const song = await Song.findById(id);
-    if (!song) {
-      return res.status(404).json({ msg: 'Song not found' });
-    }
-    res.json(song);
-  } catch (err) {
-    res.status(500).send('Server error');
-  }
-});
-
-router.delete('/:id', async (req, res) => {
+router.delete('/delete/:id', async (req, res) => {
   const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
@@ -62,4 +55,4 @@ router.delete('/:id', async (req, res) => {
   }
 });
 
-module.exports = router;
+ module.exports = router;
